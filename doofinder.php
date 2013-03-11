e767d50e384a449db4fb81d17dfc598315083389
Carlos Escribano Rey <carlos@markhaus.com>
2013-03-11 11:50:57 +0100

    Updating version numbers
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- doofinder.php
+++ doofinder.php
@@ -22,9 +22,9 @@ class Doofinder extends Module
   {
     $this->name = "doofinder";
     $this->tab = "search_filter";
-    $this->version = "0.1";
+    $this->version = "1.0";
     $this->author = "Doofinder (http://www.doofinder.com)";
-    $this->ps_versions_compliancy = array('min' => '1.4', 'max' => '1.6');
+    $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
 
     parent::__construct();
 
