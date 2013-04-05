afa22e634c51bca7eaa5df21d9254cface396c59
Carlos Escribano Rey <carlos@markhaus.com>
2013-04-05 14:12:55 +0200

    More comfortable packaging
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- package.sh
+++ package.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+if [ $# -lt 1 ]
+then
+  echo "Usage: $0 version"
+  exit 1
+fi
+
 mkdir doofinder
 cp *.php doofinder
 cp logo.* doofinder
 cp *.tpl doofinder
 cp -r css doofinder
 cp README.md doofinder
-zip -r doofinder.zip doofinder
+zip -r doofinder-p1.5-$1.zip doofinder
 rm -Rf doofinder
