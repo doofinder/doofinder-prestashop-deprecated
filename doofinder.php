5f9139eca18f03153745ea6532cefde2dad4e087
Carlos Escribano Rey <carlos@markhaus.com>
2013-04-05 13:45:16 +0200

    Dumping version number.
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- doofinder.php
+++ doofinder.php
@@ -22,7 +22,7 @@ class Doofinder extends Module
   {
     $this->name = "doofinder";
     $this->tab = "search_filter";
-    $this->version = "1.1.8";
+    $this->version = "1.1.9";
     $this->author = "Doofinder (http://www.doofinder.com)";
     $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
 
