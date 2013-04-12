99acdf9fbcc80d0ea1f316dc8ec1447f98fc6d17
Carlos Escribano Rey <carlos@markhaus.com>
2013-04-12 13:55:53 +0200

    Added license to the PHP code.
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- dfForm.class.php
+++ dfForm.class.php
@@ -1,4 +1,35 @@
 <?php
+/**
+ * Doofinder On-site Search Prestashop Module
+ *
+ * Author:  Carlos Escribano <carlos@markhaus.com>
+ * Website: http://www.doofinder.com / http://www.markhaus.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish and distribute copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ *     - The above copyright notice and this permission notice shall be
+ *       included in all copies or substantial portions of the Software.
+ *     - The Software shall be used for Good, not Evil.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * This Software is licensed with a Creative Commons Attribution NonCommercial
+ * ShareAlike 3.0 Unported license:
+ *
+ *       http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
 class dfForm
 {
   public static function formAction($currentIndex, $moduleName, $token)
