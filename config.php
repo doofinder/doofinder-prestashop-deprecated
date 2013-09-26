9209856563a3ed38750426549c9ad72a55293f0c
Carlos Escribano Rey <carlos@markhaus.com>
2013-09-26 11:28:22 +0200

    v1.2.2
    Better JSON config
    
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- config.php
+++ config.php
@@ -39,10 +39,26 @@ $baseUrl = dfTools::getModuleLink('feed.php');
 header("Content-Type:application/json; charset=utf-8");
 
 $languages = array();
+$configurations = array();
 $currencies = array_keys(dfTools::getAvailableCurrencies());
 
+$display_prices = (bool) dfTools::cfg('DF_GS_DISPLAY_PRICES', true);
+$prices_with_taxes = (bool) dfTools::cfg('DF_GS_PRICES_USE_TAX', true);
+
 foreach (Language::getLanguages(true) as $lang)
-  $languages[] = strtoupper($lang['iso_code']);
+{
+  $lang = strtoupper($lang['iso_code']);
+  $currency = dfTools::getCurrencyForLanguage($lang);
+
+  $languages[] = $lang;
+  $configurations[$lang] = array(
+    "language" => $lang,
+    "currency" => strtoupper($currency->iso_code),
+    "prices" => $display_prices,
+    "taxes" => $prices_with_taxes,
+  );
+}
+
 
 $cfg = array(
   "platform" => array(
@@ -55,9 +71,8 @@ $cfg = array(
     "options" => array(
       "language" => $languages,
       "currency" => $currencies,
-      "prices" => (bool) dfTools::cfg('DF_GS_DISPLAY_PRICES', true),
-      "taxes" => (bool) dfTools::cfg('DF_GS_PRICES_USE_TAX', true),
     ),
+    "configuration" => $configurations,
   )
 );
 
