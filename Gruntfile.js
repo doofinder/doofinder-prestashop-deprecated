1d6c63247d97043c4bc1092c71f4d5e918d9f3d3
Carlos <carlosescri@gmail.com>
2014-06-04 11:16:06 +0200

    Grunt: create release
    Signed-off-by: Carlos <carlosescri@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,52 @@ module.exports = function(grunt) {
         })(localconfig.sites);
 
     grunt.initConfig({
+        packageconfig: grunt.file.readJSON('package.json'),
+
         copy: {
             sync: {
                 files: sites_files
+            },
+            release: {
+                files: [
+                    {expand: true, src: '*.php', dest: 'release/doofinder'},
+                    {expand: true, src: '*.tpl', dest: 'release/doofinder'},
+                    {expand: true, src: '*.md', dest: 'release/doofinder'},
+                    {expand: true, src: 'logo.*', dest: 'release/doofinder'},
+
+                    {expand: true, src: 'css/**', dest: 'release/doofinder'},
+                    {expand: true, src: 'translations/**', dest: 'release/doofinder'}
+                ]
+            },
+            latest_to_version: {
+                files: [
+                    {src: 'doofinder-p1.5-latest.zip', dest: 'doofinder-p1.5-<%= packageconfig.version %>.zip'}
+                ]
             }
+        },
+        compress: {
+            release: {
+                options: {
+                    archive: 'doofinder-p1.5-latest.zip'
+                },
+                files: [
+                    {expand: true, cwd: 'release', src: '**/*'}
+                ]
+            }
+        },
+        clean: {
+            options: {
+                force: true
+            },
+            release: ['release']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-version');
+
     grunt.registerTask('default', ['copy:sync']);
+    grunt.registerTask('release', ['copy:release', 'compress:release', 'copy:latest_to_version', 'clean:release']);
 };
