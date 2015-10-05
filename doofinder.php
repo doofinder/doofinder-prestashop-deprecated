9c1ff4193c562833b61363d1f67093ac2042bad8
ecoslado <kike@doofinder.com>
2015-10-05 13:12:19 +0200

    New release.

--- doofinder.php
+++ doofinder.php
@@ -43,7 +43,7 @@ class Doofinder extends Module
 
   const GS_SHORT_DESCRIPTION = 1;
   const GS_LONG_DESCRIPTION = 2;
-  const VERSION = "2.0.0";
+  const VERSION = "2.0.1";
 
   const YES = 1;
   const NO = 0;
