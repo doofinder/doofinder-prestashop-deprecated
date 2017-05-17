258ce5a76de6747c428ccdaa909ba8ba83bab6c2
ecoslado <kike@doofinder.com>
2017-05-17 09:49:09 +0200

    Attributes selector

--- doofinder.php
+++ doofinder.php
@@ -44,7 +44,7 @@ class Doofinder extends Module
 
   const GS_SHORT_DESCRIPTION = 1;
   const GS_LONG_DESCRIPTION = 2;
-  const VERSION = "2.2.2";
+  const VERSION = "2.2.3";
   const YES = 1;
   const NO = 0;
 
